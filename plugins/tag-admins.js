17bc2b8a757266b9944627db34bdd19557d42432
𝗔𝗡𝗢𝗡𝗠𝗔𝗗 𝗡𝗘𝗧𝗪𝗢𝗥𝗞 <anonymousmad74@gmail.com>
2026-03-26 14:01:26 +0300

    Update tag-admins.js

--- plugins/tag-admins.js
+++ plugins/tag-admins.js
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, participants, reply, isGroup, senderNumber, groupAd
             teks += `${randomEmoji} @${admin.split('@')[0]}\n`;
         }
 
-        teks += "└──✪ 𝚳𝐒𝚵𝐋𝚫-𝐂𝚮𝐔𝚰 ┃ 𝚾𝚳𝐃 ✪──";
+        teks += "└──✪ 𝐊𝐀𝐋𝐈 ┃ 𝚾𝚳𝐃 ✪──";
 
         conn.sendMessage(from, { text: teks, mentions: admins }, { quoted: mek });
 
